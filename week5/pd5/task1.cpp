79aa3b426c770f511c415dbd141d2b6861c8220f
M Ahmed Butt <144407474+Ahmed8881@users.noreply.github.com>
2023-10-06 18:59:16 +0500

    Add files via upload

---
+++ week5/pd5/task1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+int value1=10;
+int value2=20;
+	
+int sum(){
+
+	value1=40;
+	return value1 = value2;
+}
+main(){
+int x= value1;
+value1=100;
+x=20;
+value2=sum();
+cout<<value1<<" "<<value2;
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
